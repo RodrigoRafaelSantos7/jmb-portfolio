aab5cb2fc49f9091d2955b6f9140e12ea3fc11fe
Rodrigo Santos <rodrigosantos@Rodrigos-MacBook-Pro.local>
2025-09-05 12:19:09 +0100

    feat(Analytics): Add Vercel Analytics to site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,23 @@
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'astro/config'
-import icon from 'astro-icon'
-import siteConfig from './src/data/site-config'
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+import icon from "astro-icon";
+import siteConfig from "./src/data/site-config";
 
 export default defineConfig({
-    site: siteConfig.website,
-    vite: {
-        plugins: [tailwindcss()]
-    },
-    integrations: [
-        mdx(),
-        sitemap(),
-        icon({
-            include: {
-                ri: ['arrow-right-up-line']
-            }
-        })
-    ]
-})
+  site: siteConfig.website,
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [
+    mdx(),
+    sitemap(),
+    icon({
+      include: {
+        ri: ["arrow-right-up-line"],
+      },
+    }),
+  ],
+});
